3b7e1c1f684865ae58cd0ec71f6fa6c846140c1c
SoftwareBuildINT <software@buildint.co>
2023-12-07 13:01:36 +0530

    updated

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.static('public'));
 
-const corsOptions = {
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: 'Content-Type,Authorization',
-};
+// const corsOptions = {
+//   origin: '*',
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+//   allowedHeaders: 'Content-Type,Authorization',
+// };
 
 app.use(cors(corsOptions));
 
@@ -61,7 +61,6 @@ app.post('/login', (req, res) => {
     });
     // Update the database with the JWT token
     res.status(200).json({ "token": token, });
-    res.header('Access-Control-Allow-Origin', '*');
   });
 });
 // Verify OTP and log in
